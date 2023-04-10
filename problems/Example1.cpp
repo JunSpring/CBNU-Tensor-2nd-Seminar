95456ea1f2de3fdde1d534140c86fc39b87283b3
JunSpring <wjdwnsqja08@gmail.com>
2023-04-10 17:31:31 +0900

    BOJ

---
+++ problems/Example1.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Person 
+{
+    string name;
+    int age;
+    string address;
+};
+
+void setValueByValue(Person p)
+{
+    p.age = 30;
+}
+
+void setValueByAddress(Person* p) 
+{
+    p->age = 40;
+}
+
+void printValueByReference(const Person& p) 
+{
+    cout << "Name: " << p.name << ", Address: " << p.address << endl;
+}
+
+int main() 
+{
+    Person p = { "John", 20, "New York" };
+    cout << "Before setValueByValue - Age: " << p.age << endl;
+    setValueByValue(p);
+    cout << "After setValueByValue - Age: " << p.age << endl;
+
+    cout << "Before setValueByAddress - Age: " << p.age << endl;
+    setValueByAddress(&p);
+    cout << "After setValueByAddress - Age: " << p.age << endl;
+
+    cout << "Before printValueByReference - Name: " << p.name << ", Address: " << p.address << endl;
+    printValueByReference(p);
+    cout << "After printValueByReference - Name: " << p.name << ", Address: " << p.address << endl;
+
+    return 0;
+}
